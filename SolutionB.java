f2b3e1cb0b64d29d4586b596070f999119aceee8
Pratibha-Patial <54446508+Pratibha-Patial@users.noreply.github.com>
2020-01-25 13:12:08 +0530

    Add files via upload

---
+++ SolutionB.java
@@ -0,0 +1,133 @@
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class SiteStats {
+    private String url;
+    private int numVisits;
+
+    /**
+     * Constructor for the SiteStats class
+     * 
+     * @param url
+     *            String that represents an URL that the user has visited
+     * @param numVisits
+     *            An int that represents the number of times that the user has
+     *            visited the url
+     */
+    public SiteStats(String url, int numVisits) {
+        this.url = url;
+        this.numVisits = numVisits;
+    }
+
+    /**
+     * This method returns the number of times that the user has visited the url.
+     * 
+     * @return An int that represents the number of times that the user has visited
+     *         the url
+     */
+    public int getNumVisits() {
+        return this.numVisits;
+    }
+    public void incVisit()
+    {
+        this.numVisits ++ ;
+    }
+
+    /**
+     * This method returns the url that we are currently tracking
+     * 
+     * @return A String that represents the url that we are currently tracking
+     */
+    public String getUrl() {
+        return this.url;
+    }
+
+    /**
+     * This method updates the number of times that we have visited the url
+     * 
+     * @param an
+     *            int that represents the number that we want to set numVisits to
+     */
+    public void setNumVisits(int updatedNumVisits) {
+        this.numVisits = updatedNumVisits;
+    }
+
+    public String toString() {
+        return this.url + " | " + this.numVisits;
+    }
+
+}
+
+public class SolutionB{
+
+    private static Queue<SiteStats> sites = new LinkedList<SiteStats>();
+
+
+    // Main method to list top n visited sites
+    public static void listTopVisitedSites(Queue<SiteStats> sites, int n) {
+        // WRITE CODE HERE
+        if(!sites.isEmpty())
+        {
+            int max = 1;
+        for(SiteStats s : sites)
+        {
+            if(max < s.getNumVisits())
+            {
+                max = s.getNumVisits();
+            }
+        }
+        while(n!=0 && max>1)
+        {
+            for(SiteStats s : sites)
+            {
+                if(s.getNumVisits()==max && n!=0)
+                {
+                    System.out.println(s);
+                    n--;
+                }
+            }
+            max--;
+        }
+        }
+        else 
+        {
+            System.out.println("No Browsing History");
+        }
+    }
+
+    // Method to find the website in the queue and increment the visited count by 1, adding new node in case website is not found
+    public static void updateCount(String url) {
+        // WRITE CODE HERE
+        boolean temp = false ;
+        for(SiteStats s : sites)
+        {
+        	if(s.getUrl().equals(url))
+        	{
+        		temp = true ;
+        		s.incVisit() ;
+        	}
+        }
+        if(!temp)
+        {
+        	SiteStats s = new SiteStats(url,1);
+        	sites.add(s);
+        }
+        
+    }
+
+    public static void main(String[] args) {
+        String[] visitedSites = { "www.google.co.in", "www.google.co.in", "www.facebook.com", "www.upgrad.com", "www.google.co.in", "www.youtube.com",
+                "www.facebook.com", "www.upgrad.com", "www.facebook.com", "www.google.co.in", "www.microsoft.com", "www.9gag.com", "www.netflix.com",
+                "www.netflix.com", "www.9gag.com", "www.microsoft.com", "www.amazon.com", "www.amazon.com", "www.uber.com", "www.amazon.com",
+                "www.microsoft.com", "www.upgrad.com" };
+
+        for (String url : visitedSites) {
+            updateCount(url);
+        }
+        listTopVisitedSites(sites, 5);
+
+    }
+
+}
